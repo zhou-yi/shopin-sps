5d7f7d29b9babde8cefa99a83f57772b2d58c664
petrozzinha <petrozzinha@gmail.com>
2010-12-19 16:44:05 +0000

    come?o das encomendas Grafical para todos

--- src/loja/thinlet/WEncomendaXML.java
+++ src/loja/thinlet/WEncomendaXML.java
@@ -4,6 +4,7 @@ package loja.thinlet;
 
 import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.util.LinkedList;
 
 import javax.swing.JOptionPane;
 
@@ -19,11 +20,25 @@ import utilities.Data;
 public class WEncomendaXML extends Thinlet{
 
 	private BaseDados b_dados = null;
-
+	private LinkedList<Produto> lista_produtos = null;
 	public WEncomendaXML(BaseDados b_dados) throws IOException {
 
 		add(parse("janelaencomenda.xml"));
 		this.b_dados=b_dados;
+	}
+	
+	
+	public void iniciaComboBox(Object combo_box) {
+
+		for(Produto p : lista_produtos){
+			//			setInteger(combo_box, "selected", lista_produtos.indexOf(p));
+			System.out.println(p.getNome()+"");
+			Object choice = create("choice");
+			setString(choice,"text", p.getNome());
+			add(combo_box, choice);
+
+		}
+
 	}
 	
 }
